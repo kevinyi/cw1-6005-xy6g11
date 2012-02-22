90944e1c336ebba9a2c5a12ad47b954a67f9aa31
Xiaojian Yi <xy6h11@ecs.soton.ac.uk>
2012-02-22 23:46:43 +0800

    Modifying file index.php

--- rssreader-1.0.0/index.php
+++ rssreader-1.0.0/index.php
@@ -46,7 +46,7 @@
 
 	# Read config from file
 	if (@!$config) {
-		$config_file = 'feeds.conf';
+		$config_file = '/etc/feeds.conf';
 		if (file_exists($config_file)) {
 			$config = conf_from_file($config_file);
 		}
